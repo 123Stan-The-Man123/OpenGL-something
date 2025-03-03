4b58fb5f328be39065330ae08541a060d2b48183
123Stan-The-Man123 <Molsas2020@gmail.com>
2025-03-03 20:59:04 +0000

    Going 3D!

--- shaders/fragment_shader.frag
+++ shaders/fragment_shader.frag
@@ -1,5 +1,4 @@
 #version 330 core
-in vec3 ourColor;
 in vec2 TexCoord;
 
 out vec4 FragColor;
@@ -9,6 +8,6 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 
 void main() {
-    FragColor = mix(texture(texture1, TexCoord) * vec4(ourColor, 1.0f), texture(texture2, TexCoord), 0.2f);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2f);
     //FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
 }
--- src/main.c
+++ src/main.c
@@ -16,27 +16,80 @@
 
 int main(void) {
     GLFWwindow *window = init_window(800, 600, "Test"); 
-    float vertices[] = {
+    /* float vertices[] = {
          0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
          0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
         -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
         -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f
+    }; */
+
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
     };
-    
-    unsigned int indices [] = {
+
+     /*unsigned int indices [] = {
         0, 1, 2,
         0, 2, 3
+    };*/
+
+    unsigned int indices [] = {
+        0, 1, 2, 2, 3, 0,
+        4, 5, 6, 6, 7, 4,
+        8, 9, 10, 10, 11, 8,
+        12, 13, 14, 14, 15, 12,
+        16, 17, 18, 18, 19, 16,
+        20, 21, 22, 22, 23, 20
+    };    
+
+    vec3 cubePositions[] = {
+        {0.0f, 0.0f, 0.0f},
+        {2.0f, 5.0f, -15.0f},
+        {-1.5f, -2.2f, -2.5f},
+        {-3.8f, -2.0f, -12.3f},
+        {2.4f, -0.4f, -3.5f},
+        {-1.7f, 3.0f, -7.5f},
+        {1.3f, -2.0f, -2.5f},
+        {1.5f, 2.0f, -2.5f},
+        {1.5f, 0.2f, -1.5f},
+        {-1.3f, 1.0f, -1.5f}
     };
     
     unsigned int VAO = create_vertex_array(vertices, sizeof(vertices), indices, sizeof(indices), GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
+    glEnable(GL_DEPTH_TEST);
 
     unsigned int shaderProgram = create_shader_program("./shaders/vertex_shader.vert", "./shaders/fragment_shader.frag");
     glUseProgram(shaderProgram);
@@ -46,25 +99,31 @@ int main(void) {
     unsigned int containerTexture = create_texture("./textures/container.jpg", GL_TEXTURE0, GL_RGB);
     unsigned int awesomeFaceTexture = create_texture("./textures/awesomeface.png", GL_TEXTURE1, GL_RGBA);
 
-    unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
+    unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
+    unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
+    unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
     
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         /* float timeValue = glfwGetTime();
         float value = (sin(timeValue) / 2.0f) + 0.5f;
         int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
         glUniform4f(vertexColorLocation, value, value, 1.0f, 1.0f);*/
 
-        mat4 trans;
-        glm_mat4_identity(trans);
-        apply_translation(trans, 0.2f, 0.3f, 0.0f);
-        apply_rotation(trans, (float)glfwGetTime(), 0.0f, 0.0f, 1.0f);
+        mat4 view;
+        glm_mat4_identity(view);
+        apply_translation(view, 0.0f, 0.0f, -3.0f);
+        
+        mat4 projection;
+        glm_mat4_identity(projection);
+        glm_perspective(glm_rad(45.0f), 800.0f / 600.0f, 0.1f, 100.0f, projection);
 
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, (const GLfloat *)trans);
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, (const GLfloat *)view);
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, (const GLfloat *)projection);
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, containerTexture);
@@ -72,7 +131,16 @@ int main(void) {
         glBindTexture(GL_TEXTURE_2D, awesomeFaceTexture);
 
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        for (unsigned int i = 0; i < 10; i++) {
+            mat4 model;
+            glm_mat4_identity(model);
+            apply_translation(model, cubePositions[i][0], cubePositions[i][1], cubePositions[i][2]);
+            float angle = 20.0f * i * glfwGetTime();
+            apply_rotation(model, glm_rad(angle), 1.0f, 0.3f, 0.5f);
+            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, (const GLfloat *)model);
+            glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();
